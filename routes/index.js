5dea6e6949369267596961e479e048718f434014
LOHwebdev3 <ledonhome.webdev3@gmail.com>
2023-08-11 11:13:30 +0700

    gitflow-feature-stash: config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const files = fs.readdirSync('./src/routes');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+files.forEach((file) => {
+  if (file !== 'index.js' && file.includes('.js')) {
+    const route = require(`./${file}`);
+    router.use(route);
+  } else if (!file.includes('.js')) {
+    const lv2 = fs.readdirSync(`./src/routes/${file}`);
+    lv2.forEach((fileLv2) => {
+      if (fileLv2 !== 'index.js' && fileLv2.includes('.js')) {
+        const route = require(`./${file}/${fileLv2}`);
+        router.use(route);
+      }
+    });
+  }
+});
+router.get('/', (req, res) => {
+    res.json({
+        status: true,
+        message: 'Herk Backend',
+        data: {
+        version: '1.0.0',
+        },
+    });
 });
 
 module.exports = router;
